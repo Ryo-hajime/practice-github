88144748cd521af463b841b2b48d2f5a561a081d
Ryoichi <sgtsit118@gmail.com>
2023-12-18 00:34:12 +0900

    追加機能

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ $("#addTodo").click(function () {
     $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
   
     $("input").val("");
-    console.log("追加した部分")
 });
-  
+
 $(document).on("change", "input[type=checkbox]", function () {
     if ($(this).is(":checked")) {
         $(this).parent().css("text-decoration", "line-through");
@@ -14,4 +13,5 @@ $(document).on("change", "input[type=checkbox]", function () {
         $(this).parent().css("text-decoration", "none");
         $(this).parent().css("color", "#000");
     }
+    console.log("追加した部分2")
 });
